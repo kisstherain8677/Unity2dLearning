8c2c4d7b912df2e6a0dccd6b7c4f90a5ca587c78
hao <842658636@qq.com>
2020-04-18 23:31:15 +0800

    add simple ui

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UIEnable()
+    {
+        GameObject.Find("Canvas/MainMenu/UI").SetActive(true);
+    }
+}
